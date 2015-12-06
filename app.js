532d765068dbd935394ed454bbc04169c6c6510a
mattfxyz <matt.faluotico@gmail.com>
2015-12-06 16:39:28 -0500

    makes reactive ajax request

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
-var searchBox = document.getElementById("search");
 var pics = document.querySelector(".reactions");
 
-Bacon.fromEvent(searchBox, "input").onValue(function(event) { 
-	pics.innerHTML = event.target.value;
-	console.log(event.target.value);
+var react;
+
+// this is an event stream
+var searchInput = $("#search")
+	.asEventStream("keyup")
+	.debounce(300)
+	.map(".target.value")
+	.toProperty()
+	.skipDuplicates();
+
+function req(query) {
+	return new Promise(function(res, rej) {
+		
+		$.ajax({url: "react.json"})
+		.done(function(allReactions) {
+			var r = allReactions.filter(function(reaction) {
+					return reaction.keywords.indexOf(query) != -1;
+				});
+			res(r);
+		})
+		.fail(function(err) {
+			rej(err);
+		});
+	});
+}
+
+function reactionSearch(query) {
+	if (query.length < 2) {
+		return Bacon.once([]);
+	} else {
+		return Bacon.fromPromise( req(query) );
+	}
+}
+
+var reactions = searchInput.flatMapLatest(reactionSearch);
+
+searchInput.awaiting(reactions).onValue(function(waiting) {
+	if (waiting) {
+		pics.innerHTML = "searching..."
+	}
+});
+
+reactions.onValue(function(results) {
+	if (results) {
+		pics.innerHTML = "";
+		console.log(results);
+		results.forEach(function (r) {
+			pics.innerHTML += r.url
+		});
+	}
 });
