f4bef16803aee2172b24123cac63087eca67a1bc
mattfxyz <matt.faluotico@gmail.com>
2015-12-06 22:45:55 -0500

    atom beautify

--- app.js
+++ app.js
@@ -2,53 +2,55 @@ var pics = $(".reactions");
 
 // this is an event stream
 var searchInput = $("#search")
-	.asEventStream("input")
-	.debounce(300)
-	.map(".target.value")
-	.toProperty()
-	.skipDuplicates();
+  .asEventStream("input")
+  .debounce(300)
+  .map(".target.value")
+  .toProperty()
+  .skipDuplicates();
 
 function req(query) {
-	return new Promise(function(res, rej) {
-		
-		$.ajax({url: "react.json"})
-		.done(function(allReactions) {
-			var r = allReactions.filter(function(reaction) {
-					return reaction.keywords.toLowerCase().indexOf(query) != -1;
-				});
-			res(r);
-		})
-		.fail(function(err) {
-			rej(err);
-		});
-	});
+  return new Promise(function(res, rej) {
+
+    $.ajax({
+        url: "react.json"
+      })
+      .done(function(allReactions) {
+        var r = allReactions.filter(function(reaction) {
+          return reaction.keywords.toLowerCase().indexOf(query) != -1;
+        });
+        res(r);
+      })
+      .fail(function(err) {
+        rej(err);
+      });
+  });
 }
 
 function reactionSearch(query) {
-	if (query.length < 2) {
-		return Bacon.once([]);
-	} else {
-		return Bacon.fromPromise( req( trim( query.toLowerCase() ) ) );
-	}
+  if (query.length < 2) {
+    return Bacon.once([]);
+  } else {
+    return Bacon.fromPromise(req(trim(query.toLowerCase())));
+  }
 }
 
 var reactions = searchInput.flatMapLatest(reactionSearch);
 
 searchInput.awaiting(reactions).onValue(function(waiting) {
-	if (waiting) {
-		pics.innerHTML = "searching..."
-	}
+  if (waiting) {
+    pics.innerHTML = "searching..."
+  }
 });
 
 reactions.onValue(function(results) {
-	if (results) {
-		pics.html($.map(results, function(reactionImage) {
-			return $('<img class="react" src="' + reactionImage.url + '">');
-		}));
-	}
+  if (results) {
+    pics.html($.map(results, function(reactionImage) {
+      return $('<img class="react" src="' + reactionImage.url + '">');
+    }));
+  }
 });
 
 
 function trim(string) {
-	return string.replace(/(\.| )/g, "");
+  return string.replace(/(\.| )/g, "");
 }
